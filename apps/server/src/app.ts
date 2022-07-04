68d9cdb24984d5aa79d9fa1adf4492b495ebd13d
Foreverjie <864129545@qq.com>
2022-07-04 23:15:00 +0800

    refactor

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,65 +1,64 @@
-require('dotenv').config();
-import express, {Express, NextFunction, Request, Response } from 'express';
-import config from 'config';
-import cookieParser from 'cookie-parser';
-import connectDB from './utils/connectDB';
-import morgan from 'morgan';
-import authRouter from './routes/auth.route';
+require('dotenv').config()
+import express, { Express, NextFunction, Request, Response } from 'express'
+import config from 'config'
+import cookieParser from 'cookie-parser'
+import connectDB from './utils/connectDB'
+import morgan from 'morgan'
+import authRouter from './routes/auth.route'
 import userRouter from './routes/user.route'
-import cors from 'cors';
+import cors from 'cors'
 
-const app: Express = express();
+const app: Express = express()
 
-const port = config.get('port') || 8080;
+const port = config.get('port') || 8080
 
 // 1. Body Parser
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '10kb' }))
 
 // 2. Cookie Parser
-app.use(cookieParser());
+app.use(cookieParser())
 
 // 3. Logger
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 // 4. Cors
 app.use(
   cors({
     origin: config.get<string>('origin'),
     credentials: true,
-  })
-);
+  }),
+)
 
 // 5. Routes
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
 
 // Testing
 app.get('/healthChecker', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     status: 'success',
     message: 'Welcome to TurboSite Server😂😂👈👈',
-  });
-});
+  })
+})
 
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`) as any;
-  err.statusCode = 404;
-  next(err);
-});
+  const err = new Error(`Route ${req.originalUrl} not found`) as any
+  err.statusCode = 404
+  next(err)
+})
 
 // Global Error Handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  err.status = err.status || 'error';
-  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error'
+  err.statusCode = err.statusCode || 500
 
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
-  });
-});
-
+  })
+})
 
 app.listen(port, () => {
-  connectDB();
-  console.log(`Server started on port ${port}`);
-});
+  connectDB()
+  console.log(`Server started on port ${port}`)
+})
--- apps/server/src/utils/connectDB.ts
+++ apps/server/src/utils/connectDB.ts
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import config from 'config';
+import mongoose from 'mongoose'
+import config from 'config'
 
 const dbUrl = `mongodb://${config.get('dbName')}:${config.get(
-  'dbPass'
-)}@localhost:27017/turboBack?authSource=admin`;
+  'dbPass',
+)}@localhost:27017/turboBack?authSource=admin`
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbUrl);
-    console.log('Database connected...');
+    await mongoose.connect(dbUrl)
+    console.log('dbUrl', dbUrl)
+    console.log('Database connected...')
   } catch (error: any) {
-    console.log(error.message);
-    setTimeout(connectDB, 5000);
+    console.log(error.message)
+    setTimeout(connectDB, 5000)
   }
-};
+}
 
-export default connectDB;
-
+export default connectDB
