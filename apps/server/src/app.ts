7e81a83a89ccaf2fb3cd4633a1fbea613335cabf
Foreverjie <864129545@qq.com>
2022-07-07 18:05:15 +0800

    grpc

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -7,6 +7,102 @@ import morgan from 'morgan'
 import authRouter from './routes/auth.route'
 import userRouter from './routes/user.route'
 import cors from 'cors'
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
+import { v4 as uuidv4 } from 'uuid'
+
+const PROTO_PATH = './customers.proto'
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true,
+  defaults: true,
+  oneofs: true,
+})
+
+const customersProto = grpc.loadPackageDefinition(packageDefinition)
+
+const server = new grpc.Server()
+
+const customers = [
+  {
+    id: 'a68b823c-7ca6-44bc-b721-fb4d5312cafc',
+    name: 'John Bolton',
+    age: 23,
+    address: 'Address 1',
+  },
+  {
+    id: '34415c7c-f82d-4e44-88ca-ae2a1aaa92b7',
+    name: 'Mary Anne',
+    age: 45,
+    address: 'Address 2',
+  },
+]
+
+server.addService(customersProto.CustomerService.service, {
+  getAll: (_: any, callback: any) => {
+    callback(null, { customers })
+  },
+
+  get: (call: any, callback: any) => {
+    let customer = customers.find(n => n.id == call.request.id)
+
+    if (customer) {
+      callback(null, customer)
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: 'Not found',
+      })
+    }
+  },
+
+  insert: (call: any, callback: any) => {
+    let customer = call.request
+
+    customer.id = uuidv4()
+    customers.push(customer)
+    callback(null, customer)
+  },
+
+  update: (call: any, callback: any) => {
+    let existingCustomer = customers.find(n => n.id == call.request.id)
+
+    if (existingCustomer) {
+      existingCustomer.name = call.request.name
+      existingCustomer.age = call.request.age
+      existingCustomer.address = call.request.address
+      callback(null, existingCustomer)
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: 'Not found',
+      })
+    }
+  },
+
+  remove: (call: any, callback: any) => {
+    let existingCustomerIndex = customers.findIndex(
+      n => n.id == call.request.id,
+    )
+
+    if (existingCustomerIndex != -1) {
+      customers.splice(existingCustomerIndex, 1)
+      callback(null, {})
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: 'Not found',
+      })
+    }
+  },
+})
+
+server.bind('127.0.0.1:30043', grpc.ServerCredentials.createInsecure())
+console.log('Server running at http://127.0.0.1:30043')
+server.start()
 
 const app: Express = express()
 
---
+++ apps/server/src/server.ts
@@ -0,0 +1,62 @@
+const grpc = require('@grpc/grpc-js')
+import protoLoader from '@grpc/proto-loader'
+
+const PROTO_PATH = './news.proto'
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+}
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options)
+const newsProto = grpc.loadPackageDefinition(packageDefinition)
+
+const server = new grpc.Server()
+let news = [
+  { id: '1', title: 'Note 1', body: 'Content 1', postImage: 'Post image 1' },
+  { id: '2', title: 'Note 2', body: 'Content 2', postImage: 'Post image 2' },
+]
+
+server.addService(newsProto.NewsService.service, {
+  getAllNews: (_: any, callback: any) => {
+    callback(null, news)
+  },
+  getNews: (_: any, callback: any) => {
+    const newsId = _.request.id
+    const newsItem = news.find(({ id }) => newsId == id)
+    callback(null, newsItem)
+  },
+  deleteNews: (_: any, callback: any) => {
+    const newsId = _.request.id
+    news = news.filter(({ id }) => id !== newsId)
+    callback(null, {})
+  },
+  editNews: (_: any, callback: any) => {
+    const newsId = _.request.id
+    const newsItem = news.find(({ id }) => newsId == id)
+    if (!newsItem) {
+      return
+    }
+    newsItem.body = _.request.body
+    newsItem.postImage = _.request.postImage
+    newsItem.title = _.request.title
+    callback(null, newsItem)
+  },
+  addNews: (call: any, callback: any) => {
+    let _news = { id: Date.now(), ...call.request }
+    news.push(_news)
+    callback(null, _news)
+  },
+})
+
+server.bindAsync(
+  '127.0.0.1:50051',
+  grpc.ServerCredentials.createInsecure(),
+  (error: any, port: any) => {
+    console.log('Server at port:', port)
+    console.log('Server running at http://127.0.0.1:50051')
+    server.start()
+  },
+)
