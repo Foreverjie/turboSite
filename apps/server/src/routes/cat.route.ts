1d8ccffb3eec448c6f497ec19722ffcae2f6fbd2
Foreverjie <864129545@qq.com>
2022-07-11 17:54:40 +0800

    cat list api

--- apps/server/src/routes/cat.route.ts
+++ apps/server/src/routes/cat.route.ts
@@ -1,43 +1,37 @@
 import * as trpc from '@trpc/server'
 import { createRouter } from './createRouter'
-import z from 'zod'
+import z, { string } from 'zod'
 import prisma from '../../prisma/prisma-client'
 
-let cats: Cat[] = []
-
-const Cat = z.object({
-  id: z.number(),
-  name: z.string(),
-})
-const Cats = z.array(Cat)
-
-function newId(): string {
-  return Math.floor(Math.random() * 10000).toString()
-}
-
-const deserializeUser = async ({ req, res, next }: any) => {
-  throw new trpc.TRPCError({ code: 'INTERNAL_SERVER_ERROR' })
-}
+// const deserializeUser = async ({ req, res, next }: any) => {
+//   throw new trpc.TRPCError({ code: 'INTERNAL_SERVER_ERROR' })
+// }
 
 export const cat = createRouter()
   // .middleware(deserializeUser)
-  .mutation('Get', {
-    input: z.object({ id: z.number() }),
-    output: Cat,
-    async resolve(req) {
-      const foundCat = cats.find(cat => cat.id === req.input.id)
-      if (!foundCat) {
-        throw new trpc.TRPCError({
-          code: 'BAD_REQUEST',
-          message: `could not find cat with id ${req.input.id}`,
-        })
-      }
-      return foundCat
-    },
-  })
+  // .mutation('Get', {
+  //   input: z.object({ id: z.number() }),
+  //   output: Cat,
+  //   async resolve(req) {
+  //     const foundCat = cats.find(cat => cat.id === req.input.id)
+  //     if (!foundCat) {
+  //       throw new trpc.TRPCError({
+  //         code: 'BAD_REQUEST',
+  //         message: `could not find cat with id ${req.input.id}`,
+  //       })
+  //     }
+  //     return foundCat
+  //   },
+  // })
   .query('List', {
-    output: Cats,
+    output: z.object({ name: z.string(), id: z.string() }).array(),
     async resolve() {
+      const cats = await prisma.cat.findMany({
+        select: {
+          name: true,
+          id: true,
+        },
+      })
       return cats
     },
   })
@@ -57,14 +51,11 @@ export const cat = createRouter()
       return newCat
     },
   })
-  .mutation('Delete', {
-    input: z.object({ id: z.number() }),
-    output: z.string(),
-    async resolve(req) {
-      cats = cats.filter(cat => cat.id !== req.input.id)
-      return 'success'
-    },
-  })
-
-type Cat = z.infer<typeof Cat>
-type Cats = z.infer<typeof Cats>
+// .mutation('Delete', {
+//   input: z.object({ id: z.number() }),
+//   output: z.string(),
+//   async resolve(req) {
+//     cats = cats.filter(cat => cat.id !== req.input.id)
+//     return 'success'
+//   },
+// })
