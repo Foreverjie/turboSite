1834321b69fd06214cc4918dbe0e249cd99c9416
Foreverjie <864129545@qq.com>
2022-08-09 17:47:33 +0800

    auth svc

---
+++ apps/auth-avc/cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/foreverjie/auth-svc/pkg/config"
+	"github.com/foreverjie/auth-svc/pkg/db"
+	"github.com/foreverjie/auth-svc/pkg/pb"
+	"github.com/foreverjie/auth-svc/pkg/services"
+	"github.com/foreverjie/auth-svc/pkg/utils"
+	"google.golang.org/grpc"
+)
+
+func main() {
+    c, err := config.LoadConfig()
+
+    if err != nil {
+        log.Fatalln("Failed at config", err)
+    }
+
+    h := db.Init(c.DBUrl)
+
+    jwt := utils.JwtWrapper{
+        SecretKey:       c.JWTSecretKey,
+        Issuer:          "go-grpc-auth-svc",
+        ExpirationHours: 24 * 365,
+    }
+
+    lis, err := net.Listen("tcp", c.Port)
+
+    if err != nil {
+        log.Fatalln("Failed to listing:", err)
+    }
+
+    fmt.Println("Auth Svc on", c.Port)
+
+    s := services.Server{
+        H:   h,
+        Jwt: jwt,
+    }
+
+    grpcServer := grpc.NewServer()
+
+    pb.RegisterAuthServiceServer(grpcServer, &s)
+
+    if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalln("Failed to serve:", err)
+    }
+}
---
+++ apps/auth-avc/pkg/db/db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	// "github.com/foreverjie/auth-svc/pkg/models"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+func Init(url string) *mongo.Client {
+    client, err := mongo.NewClient(options.Client().ApplyURI(url))
+    if err != nil {
+        log.Fatal(err)
+    }
+  
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    //ping the database
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Connected to MongoDB")
+    return client
+}
+
+//Client instance
+var DB *mongo.Client = Init()
+
+//getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+    collection := client.Database("golangAPI").Collection(collectionName)
+    return collection
+}
