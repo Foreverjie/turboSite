059d97036948fa22e9b9185c2b8638aa2e29b084
Foreverjie <864129545@qq.com>
2022-09-07 17:40:25 +0800

    feat(web): me page

--- apps/web/pages/user/me.tsx
+++ apps/web/pages/user/me.tsx
@@ -8,19 +8,16 @@ export default function UserProfile() {
     <>
       <div className="">
         <Avatar className="mt-4">
-            <div>
-              <AvatarImage
-                src={
-                  user?.avatar ??
-                  'https://jie-site.oss-cn-shenzhen.aliyuncs.com/avatar-man-icon-profile-placeholder-260nw-1229859850-e1623694994111.jpeg'
-                }
-                alt={user.name as string}
-              />
-              <AvatarFallback delayMs={600}>
-                {user?.name}
-              </AvatarFallback>
-            </div>
-          )}
+          <div>
+            <AvatarImage
+              src={
+                user?.avatar ??
+                'https://jie-site.oss-cn-shenzhen.aliyuncs.com/avatar-man-icon-profile-placeholder-260nw-1229859850-e1623694994111.jpeg'
+              }
+              alt={user?.name as string}
+            />
+            <AvatarFallback delayMs={600}>{user?.name}</AvatarFallback>
+          </div>
         </Avatar>
         <div className="">{user?.name}</div>
         <div className="">{user?.email}</div>
