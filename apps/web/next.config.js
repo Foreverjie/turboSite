0d627d8deefa577b188bc22cb94914694e600fe7
Foreverjie <864129545@qq.com>
2022-09-21 10:05:58 +0800

    feat(web): transpile modules

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')(['ui'])
+
+module.exports = withTM({
   reactStrictMode: true,
   experimental: {
     forceSwcTransforms: true,
@@ -7,4 +9,4 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+})
