33fc0f8ee108a9d52461a514436b748f9d111913
Foreverjie <864129545@qq.com>
2023-08-09 22:12:36 +0800

    feat(ui): label

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -122,7 +122,7 @@ function Onboarding() {
       </SignedIn>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <FormField
             control={form.control}
             name="email"
